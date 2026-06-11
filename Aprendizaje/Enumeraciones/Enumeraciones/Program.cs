e84e9dd20dfdd3054e234e1c70756598a0ebcc37
JoaquinHartwig <yanelian29@gmail.com>
2026-06-11 00:06:41 -0300

    Propiedades de Autoimplementacion

---
+++ Aprendizaje/Enumeraciones/Enumeraciones/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Enumeraciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- Aprendizaje/Propiedades de autoimplementacion/Propiedades de autoimplementacion/Program.cs
+++ Aprendizaje/Propiedades de autoimplementacion/Propiedades de autoimplementacion/Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//Son una versión modificada de de los getters y setters
+
         {
-            Console.WriteLine("Hello, World!");
+          Auto auto = new Auto();
+            auto._Modelo = "Toyota ";
+            Console.WriteLine(auto._Modelo);
         }
+    }
+    class Auto
+    {
+        string _modelo;
+        public string _Modelo { get; set; }
+        
+
+       
     }
 }
