684548061e3bc6a1036e16e843e20eb860f173aa
JoaquinHartwig <yanelian29@gmail.com>
2026-06-11 00:39:54 -0300

    Genericos
    Me gusto mucho esta funcionalidad

---
+++ Aprendizaje/Genericos/Genericos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Genericos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3 };
+            double[] doubleArary = { 1, 2, 3 };
+            string[] stringArary = { "1", "2", "3" };
+            MostrarArreglos(intArray); 
+            MostrarArreglos(doubleArary);
+            MostrarArreglos(stringArary);
+
+
+
+        }
+        public static void MostrarArreglos<Alex>(Alex[] arreglo)
+        {
+            foreach (Alex i in arreglo)
+            {
+
+                Console.Write(i + " ");
+
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
