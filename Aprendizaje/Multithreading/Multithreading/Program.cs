d994b34b2029ded68de4eb92cc8f9b35c89a2a3f
JoaquinHartwig <yanelian29@gmail.com>
2026-06-11 03:19:29 -0300

    Cree una carpeta

--- Aprendizaje/Multithreading/Multithreading/Program.cs
+++ Aprendizaje/Multithreading/Multithreading/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Threading;
 namespace Multithreading
 {
-    internal class Program
+    internal class Program //Los multihilos (multithreading) sirven para que un programa
+                           //pueda hacer varias tareas al mismo tiempo o
+                           //, más precisamente, que parezca hacerlo simultáneamente.
     {
         static void Main(string[] args) //Un hilo es un camino de ejecuccion de un programa
         {
