1d7b999f503f3534acfc8b07fd273592b597ac06
JoaquinHartwig <yanelian29@gmail.com>
2026-06-16 00:14:10 -0300

    Level 7

---
+++ Aprendizaje/The-CSharp-Guide-Player/Math/Math/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; // Declaring the variable a.
+            a = 9 - 2; // Assigning a value to a, using some math.
+            a = 3 + 3; // Another assignment.
+            int b = 3 + 1; // Declaring b and assigning a value to b all at once.
+            b = 1 + 2; // Assigning a second value to b.
+            int x =( 2 + 5) * 2;//Al poner parentesis cambia el orden de operacion
+            Console.WriteLine(x);
+            int partialResult = 2 + 5;//Forzamos q 2 + 5 suceda primero
+            int result = partialResult * 2;
+        }
+    }
+}
---
+++ Aprendizaje/The-CSharp-Guide-Player/THE CONVERT CLASS AND THE PARSE METHODS/THE CONVERT CLASS AND THE PARSE METHODS/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace THE_CONVERT_CLASS_AND_THE_PARSE_METHODS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your favorite number?");
+            string favoriteNumberText = Console.ReadLine();
+            int favoriteNumber = Convert.ToInt32(favoriteNumberText); //Conver to string a integer
+            Console.Write(favoriteNumber + " is a great number!");
+        }
+    }
+}
