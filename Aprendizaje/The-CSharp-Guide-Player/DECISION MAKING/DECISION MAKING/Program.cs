26b93f772057bbc5555169564e5c2cdf9085c247
JoaquinHartwig <yanelian29@gmail.com>
2026-06-17 21:14:00 -0300

    decision making

---
+++ Aprendizaje/The-CSharp-Guide-Player/DECISION MAKING/DECISION MAKING/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DECISION_MAKING
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 100;
+            if (score == 100) // sin llaves el programa solo interpreta a la primera linea como parte de la condicion if
+                Console.WriteLine("A+!");
+                 Console.WriteLine("Perfect score!"); // BUG!
+        }
+    }
+}
