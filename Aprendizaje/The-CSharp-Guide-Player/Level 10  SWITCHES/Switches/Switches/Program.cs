093ef7eed9de582d25048b4536ad894b6653585f
JoaquinHartwig <yanelian29@gmail.com>
2026-06-18 13:54:29 -0300

    Switches

---
+++ Aprendizaje/The-CSharp-Guide-Player/Level 10  SWITCHES/Switches/Switches/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Switches
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string text =
+           number switch //Version moderna de switch
+            {
+            0 => "zero",//produce" o "devuelve".
+                1 => "one",
+               _ => "other"
+              };
+            Console.WriteLine(text);
+        }
+    }
+}
