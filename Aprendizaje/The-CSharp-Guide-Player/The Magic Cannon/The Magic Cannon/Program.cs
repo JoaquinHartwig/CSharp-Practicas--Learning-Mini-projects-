1af78eb01543b4204bf657c2aa173084c9d2f796
JoaquinHartwig <yanelian29@gmail.com>
2026-06-19 17:55:42 -0300

    The Magic Cannon

---
+++ Aprendizaje/The-CSharp-Guide-Player/The Magic Cannon/The Magic Cannon/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace The_Magic_Cannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /*for de 1 a 100
+
+    si múltiplo de 3 y 5
+        azul
+        imprimir "i: Electric and Fire"
+
+    sino si múltiplo de 3
+        rojo
+        imprimir "i: Fire"
+
+    sino si múltiplo de 5
+        amarillo
+        imprimir "i: Electric"
+
+    sino
+        color normal
+        imprimir "i: Normal"
+
+    volver al color normal*/
+           for( int i = 1; i <= 100; i++)
+            {
+
+               if (i%3 == 0 && i% 5 ==0) //El orden importa porque if y else if funcionan de arriba hacia abajo y se detienen en la primera condición verdadera.
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{i} Electric and Fire "); //Cuando tengas varias condiciones que se superponen, siempre preguntá primero por la más específica.
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{i}  Fire ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{i} Electric ");
+                }
+               else
+                {
+                    Console.WriteLine($"{i} Normal ");
+
+                }
+
+            }
+        }
+    }
+}
