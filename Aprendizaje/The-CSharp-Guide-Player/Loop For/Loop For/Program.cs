64a24b1218d58cac0203489322959382540baffc
JoaquinHartwig <yanelian29@gmail.com>
2026-06-19 16:47:21 -0300

    for

---
+++ Aprendizaje/The-CSharp-Guide-Player/Loop For/Loop For/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Loop_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 5; x++)
+                Console.WriteLine(x);
+        }
+    }
+}
