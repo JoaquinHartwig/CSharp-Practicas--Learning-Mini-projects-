01148585370e1055f25fa55131159bdb2e36aaa8
JoaquinHartwig <yanelian29@gmail.com>
2026-06-20 02:07:50 -0300

    Array

---
+++ Aprendizaje/The-CSharp-Guide-Player/Array/Array/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numero = new int[3];
+            Console.WriteLine(numero.Length);
+        }
+    }
+}
