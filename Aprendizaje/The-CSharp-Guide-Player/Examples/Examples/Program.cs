23f16fc9b29255d47f34493abf171ca7c86397e9
JoaquinHartwig <yanelian29@gmail.com>
2026-06-20 02:36:34 -0300

    Examples

---
+++ Aprendizaje/The-CSharp-Guide-Player/Examples/Examples/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+            int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] < currentSmallest)
+                    currentSmallest = array[index];
+            }
+            Console.WriteLine(currentSmallest);
+        }
+    }
+}
