604b355df99b5dfe0034551bd93b664deb5d86fb
JoaquinHartwig <yanelian29@gmail.com>
2026-06-21 20:06:33 -0300

    Countdown

---
+++ Journey/The-CSharp-Guide-Player/Countdown/Countdown/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Countdown(10);
+
+            void  Countdown(int number)
+            {
+                if (number < 1)
+                {
+                    return;
+
+                }
+
+
+
+                Console.WriteLine(number);
+                Countdown(number - 1);
+
+
+
+
+
+            }
+        }
+    }
+}
