4df8542e877219092054cb5f87abcf37a9736721
JoaquinHartwig <yanelian29@gmail.com>
2026-06-21 19:48:57 -0300

    Recursion

--- Journey/The-CSharp-Guide-Player/Recursion/Recursion/Program.cs
+++ Journey/The-CSharp-Guide-Player/Recursion/Recursion/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(Factorial(5)); 
+            int Factorial(int number)
+            {
+                if (number == 1) return 1;
+                return number * Factorial(number - 1);
+            }
         }
     }
 }
+/*Factorial(5)
+    ↓
+5 × Factorial(4)
+        ↓
+    4 × Factorial(3)
+            ↓
+        3 × Factorial(2)
+                ↓
+            2 × Factorial(1)
+                    ↓
+                    1¨*/
