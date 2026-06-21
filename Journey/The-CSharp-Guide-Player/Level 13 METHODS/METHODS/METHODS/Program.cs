1d61ca2f38e7ba6e8d3a46c3a5295854d851f80f
JoaquinHartwig <yanelian29@gmail.com>
2026-06-21 04:04:08 -0300

    Update Program.cs

--- Journey/The-CSharp-Guide-Player/Level 13 METHODS/METHODS/METHODS/Program.cs
+++ Journey/The-CSharp-Guide-Player/Level 13 METHODS/METHODS/METHODS/Program.cs
@@ -5,9 +5,15 @@
         static void Main(string[] args)
         {
             CountToTen();
+            Count(11);
             void CountToTen()
             {
                 for (int current = 1; current <= 10; current++)
+                    Console.WriteLine(current);
+            }
+            void Count(int numberToCountTo)
+            {
+                for (int current = 1; current <= numberToCountTo; current++)
                     Console.WriteLine(current);
             }
         }
