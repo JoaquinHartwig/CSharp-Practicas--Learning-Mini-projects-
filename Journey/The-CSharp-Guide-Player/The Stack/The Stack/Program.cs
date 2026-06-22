b32480152c10524e6a7315cab9b283dd2bc4c30d
JoaquinHartwig <yanelian29@gmail.com>
2026-06-21 23:48:16 -0300

    Program

---
+++ Journey/The-CSharp-Guide-Player/The Stack/The Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace The_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                Console.WriteLine("Empieza Main");
+
+                Method1();
+
+                Console.WriteLine("Termina Main");
+            
+
+            static void Method1()
+            {
+                Console.WriteLine("Empieza Method1");
+
+                Method2();
+
+                Console.WriteLine("Termina Method1");
+            }
+
+            static void Method2()
+            {
+                Console.WriteLine("Estoy en Method2");
+            }
+        }
+    }
+}
