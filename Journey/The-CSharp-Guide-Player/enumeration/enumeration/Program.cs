78de049c3958f5596fe25841f11fcb99fb4eb6ff
JoaquinHartwig <yanelian29@gmail.com>
2026-06-22 22:04:24 -0300

    Enumerations

---
+++ Journey/The-CSharp-Guide-Player/enumeration/enumeration/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Seasons current = Seasons.Summer;
+            if (current == Seasons.Summer || current == Seasons.Winter)
+                Console.WriteLine("Happy solstice!");
+            else
+                Console.WriteLine("Happy equinox!");
+        }
+    }
+}
---
+++ Journey/The-CSharp-Guide-Player/enumeration/enumeration/Seasons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace enumeration
+{
+     enum Seasons
+    {
+        Winter,
+        Spring,
+        Summer,
+        Fall
+    }
+}
