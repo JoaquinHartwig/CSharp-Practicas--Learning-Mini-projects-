95e61687fa5bc7d95dd5e141a99c38b14c04a4ee
JoaquinHartwig <yanelian29@gmail.com>
2026-06-23 22:39:07 -0300

    tuples

---
+++ Journey/The-CSharp-Guide-Player/Tuplas/Tuplas/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Tuplas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayScore("Hola", 777, 666);
+            string[] names = new string[3] { "R2-D2", "C-3PO", "GONK" };
+            int[] points = new int[3] { 12420, 8543, -1 };
+            int[] level = new int[3] { 15, 9, 1 };
+           // (string, int, int) score = ("R2-D2", 12420, 15);
+            var score1 = ("R2-D2", 12420, 15);
+           // Console.WriteLine($"Name:{score.Item1} Level:{score.Item3} Score:{score.Item2}");
+            void DisplayScore((string Name, int Points, int Level) score)
+            {
+                Console.WriteLine(
+                $"Name:{score.Name} Level:{score.Level} Score:{score.Points}");
+            }
+        }
+    }
+}
